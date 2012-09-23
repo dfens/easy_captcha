7153d913fa02fb5d1760bf6de535d6000c220726
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-09-23 16:34:23 +0200

    refactor EasyCaptcha::Captcha

--- lib/easy_captcha/captcha.rb
+++ lib/easy_captcha/captcha.rb
@@ -1,18 +1,22 @@
 require 'chunky_png'
 
 module EasyCaptcha
-
   module Captcha
-
     LETTER_WIDTH  = 25
     LETTER_HEIGHT = 50
 
     def self.add_letter(img, pos, letter_num, distortion)
-      letter = ChunkyPNG::Image.from_blob(File.read("#{File.dirname(__FILE__) }/../../assets/letters/#{"%02d" % letter_num}.png"))
-      for i in 0..letter.width-1
-        for j in 0..letter.height-1
+      letter_file = File.read("#{File.dirname(__FILE__) }/../../assets/letters/#{"%02d" % letter_num}.png")    
+      letter      = ChunkyPNG::Image.from_blob(letter_file)
+
+      (0..letter.width-1).each do |i|
+        (0..letter.height-1).each do |j|
           distort = distortion ? rand(2) : 0
-          img[ i + pos * LETTER_WIDTH , j + (img.height-letter.height) / 2 + distort] = letter[i,j]
+          
+          vertical_position   = i + pos * LETTER_WIDTH
+          horizontal_position = j + (img.height - letter.height) / 2 + distort
+          
+          img[vertical_position, horizontal_position] = letter[i, j]
         end
       end
     end
@@ -20,17 +24,21 @@ module EasyCaptcha
     def self.create(text, options)
       raise "Invalid EasyCaptcha text input" unless text =~ /^[a-zA-Z]+$/
 
-      text.downcase!
+      text  = text.downcase
       width = text.length * LETTER_WIDTH
-
-      png = ChunkyPNG::Image.new(width, LETTER_HEIGHT, ChunkyPNG::Color::TRANSPARENT)
+      png   = ChunkyPNG::Image.new(width, LETTER_HEIGHT, ChunkyPNG::Color::TRANSPARENT)
 
       # Add chars
-      text.chars.each_with_index { |c, i| add_letter(png, i, c.ord - 'a'.ord, options[:distortion])}
+      text.chars.each_with_index do |c, i|
+        add_letter(png, i, c.ord - 'a'.ord, options[:distortion])
+      end
 
       if options[:snow]
         (Math.sqrt(width * LETTER_HEIGHT)).to_i.times do
-          png[rand(width), rand(LETTER_HEIGHT)] = ChunkyPNG::Color.rgb(0,0,0)
+          vertical_position   = rand(width)
+          horizontal_position = rand(LETTER_HEIGHT)
+          
+          png[vertical_position, horizontal_position] = ChunkyPNG::Color.rgb(0,0,0)
         end
       end
 
